4d4186f29788b2da7905690a263845e9b87162c2
BELDA GALBIS <serbelga@dsic.upv.es>
2018-12-10 11:43:01 +0100

    Add references

--- AprenderIdiomaApp/Form1.cs
+++ AprenderIdiomaApp/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,17 @@ namespace AprenderIdiomaApp
 {
     public partial class Form1 : Form
     {
+        private SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            synth.Speak("Inicializando la aplicación");
         }
     }
 }
