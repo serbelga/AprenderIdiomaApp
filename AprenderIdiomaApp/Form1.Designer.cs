a1543a574ca630dffb2a0aeb913dc2ace659301a
alumno <serbelga@inf.upv.es>
2018-12-10 20:37:42 +0100

    Building Grammar

--- AprenderIdiomaApp/Form1.Designer.cs
+++ AprenderIdiomaApp/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.question = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.correct = new System.Windows.Forms.Label();
+            this.questionStatement = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // question
+            // 
+            this.question.AutoSize = true;
+            this.question.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question.Location = new System.Drawing.Point(755, 9);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(20, 24);
+            this.question.TabIndex = 0;
+            this.question.Text = "1";
+            this.question.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(658, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Question:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "LearnApp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(658, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Correct:";
+            // 
+            // correct
+            // 
+            this.correct.AutoSize = true;
+            this.correct.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.correct.Location = new System.Drawing.Point(755, 33);
+            this.correct.Name = "correct";
+            this.correct.Size = new System.Drawing.Size(20, 24);
+            this.correct.TabIndex = 4;
+            this.correct.Text = "1";
+            // 
+            // questionStatement
+            // 
+            this.questionStatement.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.questionStatement.AutoSize = true;
+            this.questionStatement.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionStatement.Location = new System.Drawing.Point(334, 283);
+            this.questionStatement.Name = "questionStatement";
+            this.questionStatement.Size = new System.Drawing.Size(112, 24);
+            this.questionStatement.TabIndex = 5;
+            this.questionStatement.Text = "La pregunta";
+            this.questionStatement.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.questionStatement);
+            this.Controls.Add(this.correct);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.question);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label question;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label correct;
+        private System.Windows.Forms.Label questionStatement;
     }
 }
 
--- AprenderIdiomaApp/Form1.cs
+++ AprenderIdiomaApp/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,6 +20,8 @@ namespace AprenderIdiomaApp
         private SpeechSynthesizer synth = new SpeechSynthesizer();
         public Form1()
         {
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
             InitializeComponent();
             this.Load += Form1_Load;
         }
@@ -25,11 +29,10 @@ namespace AprenderIdiomaApp
         public void Form1_Load(object sender, EventArgs e)
         {
             synth.Speak("Inicializando la aplicación");
-
             Grammar grammar = CreateGrammarBuilderSemantics(null);
             _recognizer.SetInputToDefaultAudioDevice();
             _recognizer.UnloadAllGrammars();
-            _recognizer.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", 60);
+            _recognizer.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", 40);
             grammar.Enabled = true;
             _recognizer.LoadGrammar(grammar);
             _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_recognizer_SpeechRecognized);
@@ -39,17 +42,63 @@ namespace AprenderIdiomaApp
 
         private void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            return;
+            SemanticValue semantics = e.Result.Semantics;
+            if (e.Result.Text.Contains("Cerrar") || e.Result.Text.Contains("Salir"))
+            {
+                System.Windows.Forms.Application.Exit();
+            } else if (e.Result.Text.Contains("Next question")) {
+                int aux = int.Parse(this.question.Text) + 1;
+                this.question.Text = aux.ToString();
+            } else if (e.Result.Semantics.ContainsKey("topics")) {
+                this.questionStatement.Text = "animales";
+            } else if (e.Result.Text.Contains("Previous")) {
+                int aux = int.Parse(this.question.Text) - 1;
+                this.question.Text = aux.ToString();
+            }
         }
 
         private Grammar CreateGrammarBuilderSemantics(object p)
         {
-            GrammarBuilder cerrar = "cerrar";
-            Choices frases = new Choices(cerrar);
-       
-            Grammar grammar = new Grammar(frases);
-            grammar.Name = "Poner/Cambiar Fondo";
+            //Close Application
+            GrammarBuilder close = "Cerrar";
+            GrammarBuilder exit = "Salir de";
+            Choices closeCh = new Choices(close, exit);
+            GrammarBuilder application = "la aplicacion";
+            GrammarBuilder closePhrase = new GrammarBuilder(closeCh);
+            closePhrase.Append(application);
+
+            //Select questions
+            Choices topicsCh = new Choices();
+            GrammarBuilder want = "Quiero cuestiones de";
+            SemanticResultValue semanticResultValue = new SemanticResultValue("animales", "animales");
+            GrammarBuilder resultValueBuilder = new GrammarBuilder(semanticResultValue);
+            topicsCh.Add(resultValueBuilder);
+            SemanticResultKey semanticResultKey = new SemanticResultKey("topics", topicsCh);
+            GrammarBuilder topics = new GrammarBuilder(semanticResultKey);
+            GrammarBuilder selectPhrase = want;
+            selectPhrase.Append(topics);
+
+            //Begin again
+            GrammarBuilder begin = "Quiero empezar de nuevo";
+
+            //Help
+            GrammarBuilder help = "Necesito ayuda";
+
+          
+            
+            Choices next = new Choices(new GrammarBuilder("Next question"));
+            Choices previous = new Choices(new GrammarBuilder("Previous question"));
+
+            Choices opciones = new Choices(begin, help, closePhrase, selectPhrase, next, previous);
+            Grammar grammar = new Grammar(opciones);
+            
+            grammar.Name = "Questions";
             return grammar;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
